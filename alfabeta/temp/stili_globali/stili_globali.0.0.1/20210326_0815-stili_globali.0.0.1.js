e7acea32ce8fb097db283a553869579ece8877d5
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-03-26 12:59:18 +0100

    Add files via upload

---
+++ alfabeta/temp/stili_globali/stili_globali.0.0.1/20210326_0815-stili_globali.0.0.1.js
@@ -0,0 +1,11 @@
+document.getElementById("stili_globali").innerHTML=
+ '<style> '+
+ 'html * {color:maroon;} '+
+ 'html * {font-size:normal;font-family:"PT Mono", monospace;} '+
+ 'html * {overflow:auto!important;} '+
+ 'html * {} '+
+ 'html * {} '+
+ ' '+
+ 'body {margin-left:2.5rem;margin-right:2.5rem;margin-bottom:15rem!important;} '+
+ '</style> '+
+ '';
